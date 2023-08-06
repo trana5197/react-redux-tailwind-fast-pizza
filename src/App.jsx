9227aae6cc4664a3141a47cd39e7a07f98eb0b8d
rana <trana51997@gmail.com>
2023-08-06 18:41:46 -0500

    Components made and tailwind css half done

---
+++ src/App.jsx
@@ -0,0 +1,49 @@
+import { RouterProvider, createBrowserRouter } from "react-router-dom";
+
+import Home from "./ui/Home";
+import Menu from "./features/menu/Menu";
+import Cart from "./features/cart/Cart";
+import CreateOrder from "./features/order/CreateOrder";
+import Order from "./features/order/Order";
+
+import AppLayout from "./ui/AppLayout";
+import Error from "./ui/Error";
+
+import { loader as menuLoader } from "./features/menu/Menu";
+import { loader as orderLoader } from "./features/order/Order";
+import { action as createOrderAction } from "./features/order/CreateOrder";
+
+const router = createBrowserRouter([
+  {
+    element: <AppLayout />,
+    errorElement: <Error />,
+
+    children: [
+      { path: "/", element: <Home /> },
+      {
+        path: "/menu",
+        element: <Menu />,
+        loader: menuLoader,
+        errorElement: <Error />,
+      },
+      { path: "/cart", element: <Cart /> },
+      {
+        path: "/order/new",
+        element: <CreateOrder />,
+        action: createOrderAction,
+      },
+      {
+        path: "/order/:orderId",
+        element: <Order />,
+        loader: orderLoader,
+        errorElement: <Error />,
+      },
+    ],
+  },
+]);
+
+function App() {
+  return <RouterProvider router={router} />;
+}
+
+export default App;
---
+++ src/ui/Header.jsx
@@ -0,0 +1,17 @@
+import { Link } from "react-router-dom";
+import SearchOrder from "../features/order/SearchOrder";
+import Username from "../features/user/Username";
+
+function Header() {
+  return (
+    <div className="flex items-center justify-between border-b  border-stone-300 bg-yellow-400 px-4 py-3 uppercase sm:px-6">
+      <Link to="/" className="font-semibold tracking-widest">
+        Fast React Pizza Co.
+      </Link>
+      <SearchOrder />
+      <Username />
+    </div>
+  );
+}
+
+export default Header;
